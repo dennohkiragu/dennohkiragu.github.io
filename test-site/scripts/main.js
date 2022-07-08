54095e42e96715721cba3d1bd02e3fd9f1b4bb28
dennohkiragu <50009421+dennohkiragu@users.noreply.github.com>
2022-07-08 08:48:04 +0300

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image.jpeg') {
+        myImage.setAttribute('src','images/image2.jpg');
+    } else {
+        myImage.setAttribute('src','images/image2.jpg');
+    }
+}
+
+let myButton =  document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please Enter your name');
+    if(!myName) {
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.textContent = 'Mozilla is cool,' +myName;
+    }
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Mozilla is cool' + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool' + storedName;
+}
+
+myButton.onclick = function () {
+setUserName();
+}
